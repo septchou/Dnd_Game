1026ae514acca75f1a3232ea3d45c66b37a0e32a
Jarukit <jarukit.pan-@mail.kmutt.ac.th>
2024-11-08 22:15:48 +0700

    Link username in database to display name
    Only in Main Menu(start Menu)

--- UnityFile/Assets/Script/Lobby/PlayerNameManager.cs
+++ UnityFile/Assets/Script/Lobby/PlayerNameManager.cs
@@ -5,26 +5,54 @@ using TMPro;
 using Photon.Pun;
 using Photon.Realtime;
 using Unity.VisualScripting;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase;
+using Firebase.Extensions;
+
 
 public class PlayerNameManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField usernameInput;
 
+    private FirebaseAuth auth;
+
     void Start()
     {
-        if (string.IsNullOrEmpty(usernameInput.text))
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
         {
-            SetRandomPlayerName();
-        }
-    }
-    public void OnUsernameInputValueChanged()
-    {
-        // Otherwise, set the nickname to the entered username
-        PhotonNetwork.NickName = usernameInput.text;
+            if (task.Result == DependencyStatus.Available)
+            {
+                auth = FirebaseAuth.DefaultInstance;
+
+                if (auth.CurrentUser != null)
+                {
+                    LoadUsername();
+                }
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + task.Result);
+            }
+        });
     }
 
-    private void SetRandomPlayerName()
+    void LoadUsername()
     {
-       PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
+        FirebaseUser user = auth.CurrentUser;
+
+        if (user != null)
+        {
+            //get the username from the display name 
+            string username = user.DisplayName;
+            usernameInput.text = username;
+        }
+        else
+        {
+                Debug.LogWarning("No user is currently logged in.");
+        }
+       
     }
+
+
 }
