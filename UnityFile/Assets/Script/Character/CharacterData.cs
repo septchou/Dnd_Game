90ef0b977f62fd62c519e117469096d2a0f3b9af
Rozene Mokem <Enezor0612@gmail.com>
2024-11-08 20:48:25 +0700

    Reapply "Fully working character creation"
    This reverts commit 619a9e6d6b6a516a4a6bd18a89bda9a98914e23b.

---
+++ UnityFile/Assets/Script/Character/CharacterData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CharacterData
+{
+    public string characterName;
+    public string raceName;
+    public string characterClassName;
+    public int level;
+    public int HP;
+    public int abilityPoint;
+    public List<AbilityScorePointsData> abilityScorePoints;
+    public List<SkillData> skillData;
+
+    [System.Serializable]
+    public class AbilityScorePointsData
+    {
+        public string abilityScoreName;
+        public int abilityScorePoint;
+        public int ablityModifierBonus;
+    }
+
+    [System.Serializable]
+    public class SkillData
+    {
+        public string skillName;
+        public string skillType;
+    }
+}
