fcfbf2c45a3dfc5c5d0f8900c45a231370f9cff0
Rozene Mokem <Enezor0612@gmail.com>
2024-11-17 01:04:27 +0700

    In progress Race and Skill Detail

--- UnityFile/Assets/Script/Character/OnMouseOverRaceandClass.cs
+++ UnityFile/Assets/Script/Character/OnMouseOverRaceandClass.cs
@@ -7,7 +7,12 @@ using TMPro;
 public class OnMouseOverRaceandClass : MonoBehaviour
 {
 
-    public GameObject raceDropdown; // Assign the dropdown GameObject in the inspector
+    [SerializeField] List<GameObject> existListItem = new List<GameObject>();
+    [SerializeField] GameObject raceDropdownObj, classDropdownObj, detailListItemPrefab, detailListPanel;
+    [SerializeField] TMP_Dropdown raceDropdown, classDropdown;
+    [SerializeField] TMP_Text headerText;
+    [SerializeField] Transform detailContent;
+
 
     void Update()
     {
@@ -15,12 +20,53 @@ public class OnMouseOverRaceandClass : MonoBehaviour
         {
             // Check if the mouse is over the specific dropdown GameObject
             if (RectTransformUtility.RectangleContainsScreenPoint(
-                raceDropdown.GetComponent<RectTransform>(),
+                raceDropdownObj.GetComponent<RectTransform>(),
                 Input.mousePosition,
                 Camera.main))
             {
-                
+                popUpRaceDetailUI();
+            }
+
+            if (RectTransformUtility.RectangleContainsScreenPoint(
+                classDropdownObj.GetComponent<RectTransform>(),
+                Input.mousePosition,
+                Camera.main))
+            {
+
+            }
+            else
+            {
+                detailListPanel.SetActive(false);
             }
         }
+    }
+
+    private void popUpRaceDetailUI()
+    {
+        foreach (GameObject item in existListItem)
+        {
+            Destroy(item);
+        }
+        existListItem = new List<GameObject>();
+
+        
+        Race race = Resources.Load<Race>("Races/" + raceDropdown.value);
+        if(race != null)
+        {
+            headerText.text = race.name;
+            foreach (Skill skill in race.raceSkills)
+            {
+                GameObject item = Instantiate(detailListItemPrefab, detailContent);
+                existListItem.Add(item);
+                item.GetComponent<SkillListItem>().SetUp(race.name, skill.detail);
+            }
+
+            RectTransform prefabRect = detailListItemPrefab.GetComponent<RectTransform>();
+            float prefabHeight = prefabRect.rect.height;
+            float totalHeight = prefabHeight * existListItem.Count;
+            RectTransform panelRect = detailListPanel.GetComponent<RectTransform>();
+            panelRect.sizeDelta = new Vector2(panelRect.sizeDelta.x, totalHeight);
+        }
+        detailListPanel.SetActive(true);
     }
 }
