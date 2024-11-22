7f7d46860c95e760827e9131226168f6df48c649
Jarukit <jarukit.pan-@mail.kmutt.ac.th>
2024-11-22 20:32:49 +0700

    Link save/load map to database
    ยังไม่ได้แก้ ปัญหาเรื่องชื่อไฟล์ map กับ ปุ่มบลาๆๆๆ

--- UnityFile/Assets/Script/Map/SaveHandler.cs
+++ UnityFile/Assets/Script/Map/SaveHandler.cs
@@ -2,6 +2,13 @@ using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using Firebase.Extensions;
+using UnityEngine.Timeline;
+using UnityEngine.InputSystem;
+using UnityEditor.U2D.Aseprite;
 
 public class SaveHandler : Singleton<SaveHandler> {
     Dictionary<string, Tilemap> tilemaps = new Dictionary<string, Tilemap>();
@@ -9,21 +16,43 @@ public class SaveHandler : Singleton<SaveHandler> {
     Dictionary<string, TileBase> guidToTileBase = new Dictionary<string, TileBase>();
 
     [SerializeField] BoundsInt bounds;
-    [SerializeField] string filename = "tilemapData.json";
+    [SerializeField] string filename = "Map";
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth auth;
+
 
     private void Start() {
-        InitTilemaps();
-        InitTileReferences();
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            if (task.Result == DependencyStatus.Available)
+            {
+                auth = FirebaseAuth.DefaultInstance;
+                databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+
+                InitTilemaps();
+                InitTileReferences();
+                Debug.Log("Firebase initialized successfully(Map).");
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies.");
+            }
+        });
     }
 
-    private void InitTileReferences() {
+    private void InitTileReferences()
+    {
         BuildingObjectBase[] buildables = Resources.LoadAll<BuildingObjectBase>("Map/Scriptables/Buildables");
 
-        foreach (BuildingObjectBase buildable in buildables) {
-            if (!tileBaseToBuildingObject.ContainsKey(buildable.TileBase)) {
+        foreach (BuildingObjectBase buildable in buildables)
+        {
+            if (!tileBaseToBuildingObject.ContainsKey(buildable.TileBase))
+            {
                 tileBaseToBuildingObject.Add(buildable.TileBase, buildable);
                 guidToTileBase.Add(buildable.name, buildable.TileBase);
-            } else {
+            }
+            else
+            {
                 Debug.LogError("TileBase " + buildable.TileBase.name + " is already in use by " + tileBaseToBuildingObject[buildable.TileBase].name);
             }
         }
@@ -42,70 +71,127 @@ public class SaveHandler : Singleton<SaveHandler> {
         }
     }
 
-    public void OnSave() {
-        // List that will later be safed
-        List<TilemapData> data = new List<TilemapData>();
+    public void OnSaveToFirebase()
+    {
+        if (auth.CurrentUser == null)
+        {
+            Debug.LogError("No user is logged in.");
+            return;
+        }
 
-        // foreach existing tilemap
-        foreach (var mapObj in tilemaps) {
-            TilemapData mapData = new TilemapData();
-            mapData.key = mapObj.Key;
+        string userId = auth.CurrentUser.UserId;
 
-            // use your boundsInt variable for the bounds
-            // alternatetively you can use mapObj.Value.cellBounds
-            // https://docs.unity3d.com/ScriptReference/Tilemaps.Tilemap-cellBounds.html
+        // List that will later be saved
+        TilemapDataListWrapper dataWrapper = new TilemapDataListWrapper();
+
+        foreach (var mapObj in tilemaps)
+        {
+            TilemapData mapData = new TilemapData
+            {
+                key = mapObj.Key
+            };
 
             BoundsInt boundsForThisMap = mapObj.Value.cellBounds;
 
-            for (int x = boundsForThisMap.xMin; x < boundsForThisMap.xMax; x++) {
-                for (int y = boundsForThisMap.yMin; y < boundsForThisMap.yMax; y++) {
+            for (int x = boundsForThisMap.xMin; x < boundsForThisMap.xMax; x++)
+            {
+                for (int y = boundsForThisMap.yMin; y < boundsForThisMap.yMax; y++)
+                {
                     Vector3Int pos = new Vector3Int(x, y, 0);
                     TileBase tile = mapObj.Value.GetTile(pos);
 
-                    if (tile != null && tileBaseToBuildingObject.ContainsKey(tile)) {
+                    if (tile != null && tileBaseToBuildingObject.ContainsKey(tile))
+                    {
                         String guid = tileBaseToBuildingObject[tile].name;
                         TileInfo ti = new TileInfo(pos, guid);
-                        // Add "TileInfo" to "Tiles" List of "TilemapData"
                         mapData.tiles.Add(ti);
                     }
                 }
             }
 
-            // Add "TilemapData" Object to List
-            data.Add(mapData);
+            dataWrapper.tilemaps.Add(mapData);
         }
-        FileHandler.SaveToJSON<TilemapData>(data, filename);
+        Debug.Log("DataWrapper: " + dataWrapper.tilemaps);
+        string json = JsonUtility.ToJson(dataWrapper);
+        Debug.Log("Generated JSON: " + json);
+
+
+        Debug.Log("UserID:" + userId);
+        // Check if mapKey exists, update or create a new one
+        databaseReference.Child("tilemap_data").Child(userId).Child(filename).SetRawJsonValueAsync(json).ContinueWithOnMainThread(task => {
+            if (task.IsCompleted)
+            {
+                Debug.Log($"Tilemap data saved successfully to Firebase with name: {filename}");
+            }
+            else
+            {
+                Debug.LogError("Failed to save tilemap data: " + task.Exception);
+                
+            }
+        });
     }
 
-    public void OnLoad() {
-        List<TilemapData> data = FileHandler.ReadListFromJSON<TilemapData>(filename);
+    public void OnLoadFromFirebase()
+    {
+        if (auth.CurrentUser == null)
+        {
+            Debug.LogError("No user is logged in.");
+            return;
+        }
 
-        foreach (var mapData in data) {
-            // if key does NOT exist in dictionary skip it
-            if (!tilemaps.ContainsKey(mapData.key)) {
-                Debug.LogError("Found saved data for tilemap called '" + mapData.key + "', but Tilemap does not exist in scene.");
-                continue;
-            }
+        string userId = auth.CurrentUser.UserId;
 
-            // get according map
-            var map = tilemaps[mapData.key];
+        databaseReference.Child("tilemap_data").Child(userId).Child(filename).GetValueAsync().ContinueWithOnMainThread(task => {
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result;
+                if (!snapshot.Exists)
+                {
+                    Debug.LogWarning("No tilemap data found for this user.");
+                    return;
+                }
 
-            // clear map
-            map.ClearAllTiles();
+                TilemapDataListWrapper dataWrapper = JsonUtility.FromJson<TilemapDataListWrapper>(snapshot.GetRawJsonValue());
 
-            if (mapData.tiles != null && mapData.tiles.Count > 0) {
-                foreach (var tile in mapData.tiles) {
 
-                    if (guidToTileBase.ContainsKey(tile.guidForBuildable)) {
-                        map.SetTile(tile.position, guidToTileBase[tile.guidForBuildable]);
-                    } else {
-                        Debug.LogError("Refernce " + tile.guidForBuildable + " could not be found.");
+                foreach (var mapData in dataWrapper.tilemaps)
+                {
+                    if (!tilemaps.ContainsKey(mapData.key))
+                    {
+                        Debug.LogError("Found saved data for tilemap called '" + mapData.key + "', but Tilemap does not exist in scene.");
+                        continue;
                     }
 
+                    var map = tilemaps[mapData.key];
+                    map.ClearAllTiles();
+
+                    if (mapData.tiles != null && mapData.tiles.Count > 0)
+                    {
+                        foreach (var tile in mapData.tiles)
+                        {
+                            if (guidToTileBase.ContainsKey(tile.guidForBuildable))
+                            {
+                                map.SetTile(tile.position, guidToTileBase[tile.guidForBuildable]);
+                            }
+                            else
+                            {
+                                Debug.LogError("Reference " + tile.guidForBuildable + " could not be found.");
+                            }
+                        }
+                    }
                 }
+
+                Debug.Log("Tilemap data loaded successfully from Firebase.");
             }
-        }
+            else
+            {
+                Debug.LogError("Failed to load tilemap data: " + task.Exception);
+            }
+        });
     }
+
+
+
 }
 
 
@@ -114,6 +200,13 @@ public class TilemapData {
     public string key; // the key of your dictionary for the tilemap - here: the name of the map in the hierarchy
     public List<TileInfo> tiles = new List<TileInfo>();
 }
+
+[Serializable]
+public class TilemapDataListWrapper
+{
+    public List<TilemapData> tilemaps = new List<TilemapData>();
+}
+
 
 [Serializable]
 public class TileInfo {
