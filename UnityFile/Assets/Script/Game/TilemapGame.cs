12c07f706e3a581440326081644beb38defe3a75
palm9086 <118614294+palm9086@users.noreply.github.com>
2024-11-23 20:56:57 +0700

    Add Tilemap to GameSetUp
    Add Tilemap

---
+++ UnityFile/Assets/Script/Game/TilemapGame.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGame : Singleton<TilemapGame>
+{
+    [SerializeField] List<BuildingCategory> categoriesToCreateTilemapFor;
+    [SerializeField] Transform grid;
+
+    private void Start()
+    {
+        CreateMaps();
+    }
+
+    private void CreateMaps()
+    {
+        foreach (BuildingCategory category in categoriesToCreateTilemapFor)
+        {
+            // Create new GameObject
+            GameObject obj = new GameObject("Tilemap_" + category.name);
+
+            // Assign Tilemap Features
+            Tilemap map = obj.AddComponent<Tilemap>();
+            TilemapRenderer tr = obj.AddComponent<TilemapRenderer>();
+
+            // Set Parent
+            obj.transform.SetParent(grid);
+
+            // Set sorting order
+            tr.sortingOrder = category.SortingOrder;
+
+            // Save reference to the Tilemap in the category
+            category.Tilemap = map;
+
+            // Add collider and other components based on category
+            if (category.name == "Wall")
+            {
+                AddWallCollider(obj);
+            }
+        }
+    }
+
+    private void AddWallCollider(GameObject tilemapGameObject)
+    {
+        // Add TilemapCollider2D
+        TilemapCollider2D tilemapCollider = tilemapGameObject.AddComponent<TilemapCollider2D>();
+
+        // Add a Rigidbody2D to freeze the tilemap in place
+        Rigidbody2D rb = tilemapGameObject.AddComponent<Rigidbody2D>();
+    
+        // Set the Rigidbody to be kinematic so it doesn't react to physics, and freeze it on all axes
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        Debug.Log("Wall Tilemap with Collider and Rigidbody added: " + tilemapGameObject.name);
+    }
+    
+}
