19dde6af3770fd1c2f6c649eabc9adeb567e56d9
Rozene Mokem <Enezor0612@gmail.com>
2024-11-23 19:05:12 +0700

    Master client can now switch back and forth between map and lobby scene

---
+++ UnityFile/Assets/Script/Lobby/MapSwitch.cs
@@ -0,0 +1,69 @@
+using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapSwitch : MonoBehaviour
+{
+    public static MapSwitch Instance;
+    public bool isSwitchMap;
+    public Button switchMapButton;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+
+        isSwitchMap = false;
+
+        
+
+    }
+
+
+    public void ChangeToMapScene(string sceneName)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.AutomaticallySyncScene = false;
+            Hashtable customProperties = new Hashtable
+            {
+                { "MasterScene", sceneName }
+            };
+            PhotonNetwork.CurrentRoom.SetCustomProperties(customProperties);
+
+
+            isSwitchMap = true;
+
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+
+        }
+    }
+
+    public void ReassignButton()
+    {
+        GameObject buttonObject = GameObject.Find("MapButton");
+        if (buttonObject != null)
+        {
+            switchMapButton = buttonObject.GetComponent<Button>();
+            switchMapButton.onClick.AddListener(() => ChangeToMapScene("MapMaker"));
+        }
+        else
+        {
+            Debug.LogWarning("Button not found in the new scene.");
+        }
+    }
+
+}
